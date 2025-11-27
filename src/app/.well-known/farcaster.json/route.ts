52cabf180ea43397edb366fbbc65e5603dc64b1e
0xDeepanshu <wtfdeep@duck.com>
2025-11-28 00:39:35 +0530

    manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -15,8 +15,15 @@ export async function GET() {
       primaryCategory: "game with Gx402",
       buttonTitle: "Launch ",
       splashImageUrl: `${APP_URL}/images/icon.png`,
-      splashBackgroundColor: "#ffffff",
+      splashBackgroundColor: "#000000",
+      subtitle: "play and stack",
+      description: "StackMon is a fast, addictive tile-stacking game where every perfect drop brings you closer to earning tokens. Build the tallest tower you can, beat your high score, and claim rewards based on skill — not luck."
     },
+    accountAssociation: {
+    header: "eyJmaWQiOjEzNzUzNjIsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg4RDdiQzIxMDM2ZkZCQkU3NjQ3MEM1Yjk4QTFhNDBkOTI5M0E2NjAyIn0",
+    payload: "eyJkb21haW4iOiJ3d3cuc3RhY2ttb24ueHl6In0",
+    signature: "8UjnwjtndgOMnhdUhSqumbHhXI35iaCpFcKhXPwkw494nNBWWQc1NjfH1H/Z0M5bQawqgaxpNMJ4TKv4IvJmDxs="
+  }
   };
 
   return NextResponse.json(farcasterConfig);
